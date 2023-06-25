8649d3356503be68f79e4f25985198b3df4116d9
AKASH BABU <akashk29940@gmail.com>
2023-06-25 15:31:16 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    double volume() {
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter width : ");
+        double width = s.nextDouble();
+        System.out.print("enter height : ");
+        double height = s.nextDouble();
+        System.out.print("enter depth: ");
+        double depth = s.nextDouble();
+        return width * height * depth;
+    }
+}
+
+class ReturnDemo {
+    public static void main(String args[]) {
+        Box cuboid = new Box();
+        double vol;
+        vol = cuboid.volume();
+        System.out.println("Volume is " + vol);
+    }
+}
